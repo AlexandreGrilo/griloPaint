c4a81b273e3c0d71299686d4909c88eabbe07a3c
Alexandre Vaz <alexandregriloskt@gmail.com>
2018-10-18 10:40:09 +0100

    Start Paint. gitignore

---
+++ src/org/academiadecodigo/bootcamp/Main.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 18/10/2018.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/academiadecodigo/bootcamp/Paint.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp;
+
+/**
+ * Created by codecadet on 18/10/2018.
+ */
+public class Paint {
+}
